a513cbc94f0c127d6bf4a5a01b2f655c72001897
matias <maty_colazo@hotmail.com>
2023-11-23 15:52:15 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { TareasFormComponent } from './components/tareas-form/tareas-form.component';
+import { TareasListComponent } from './components/tareas-list/tareas-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TareasComponent,
+    TareasFormComponent,
+    TareasListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tareas-list/tareas-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas-list',
+  templateUrl: './tareas-list.component.html',
+  styleUrls: ['./tareas-list.component.css']
+})
+export class TareasListComponent {
+
+}
