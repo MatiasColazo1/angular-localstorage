8b98048002e0d4181fc1d2a87ec52a00817fa4ba
matias <maty_colazo@hotmail.com>
2023-11-23 16:53:27 -0300

    actualizacion

--- src/app/components/tareas-form/tareas-form.component.ts
+++ src/app/components/tareas-form/tareas-form.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-tareas-form',
   templateUrl: './tareas-form.component.html',
   styleUrls: ['./tareas-form.component.css']
 })
-export class TareasFormComponent {
+export class TareasFormComponent implements OnInit {
+ 
+   constructor() { }
 
+   ngOnInit() {
+    
+   }
+
+   addTarea(newTitulo:HTMLInputElement, newDescripcion:HTMLTextAreaElement) {
+    console.log("agregando", newTitulo.value, newDescripcion.value);
+    return false;
+   }
 }
--- src/app/services/tareas.service.ts
+++ src/app/services/tareas.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Tareas } from '../models/tareas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareasService {
 
-  constructor() { }
+  tareas: Tareas[];
+
+  constructor() { 
+    this.tareas = [
+      {titulo: 'leer', descripcion: 'Tengo que leer'},
+      {titulo: 'website', descripcion: 'Crear un sitio web'},
+    ]
+   }
+
+   getTareas() {
+    return this.tareas;
+   }
+
+   addTareas(tareas: Tareas) {
+    this.tareas.push(tareas)
+   }
 }
